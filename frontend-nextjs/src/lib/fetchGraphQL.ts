3edf83adf73876e33930721617b7b82596ae428e
Anwar <ak2211.g@gmail.com>
2025-06-11 12:23:46 +0800

    Sign in and Sign Up logic and code, as well the new backend with fixing the previce issue

---
+++ frontend-nextjs/src/lib/fetchGraphQL.ts
@@ -0,0 +1,52 @@
+"use server";
+import { BACKEND_URL } from "./constants";
+import { getSession } from "./session";
+
+export const fetchGraphQL = async (query: string, variables = {}) => {
+  console.log("######################################");
+  console.log("######################################");
+  console.log(query);
+  console.log(variables);
+  console.log("######################################");
+  console.log("######################################");
+  const response = await fetch(`${BACKEND_URL}/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+  const result = await response.json();
+  if (result.errors) {
+    console.error("GraphQL errors:", result.errors);
+    throw new Error("Failed to fetch the data from GraphQL");
+  }
+
+  return result.data;
+};
+
+export const authFetchGraphQL = async (query: string, variables = {}) => {
+  const session = await getSession();
+  const response = await fetch(`${BACKEND_URL}/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${session?.accessToken}`,
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  const result = await response.json();
+  if (result.errors) {
+    console.error("GraphQL errors:", result.errors);
+    throw new Error("Failed to fetch the data from GraphQL");
+  }
+
+  return result.data;
+};
